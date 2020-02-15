a36c3bafde34bc338e6cc7310d92b1b3bb314492
Denis <den-sidnay@yandex.ru>
2020-02-15 19:23:20 +0300

    add about page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
+$team = [
+    ['name' => 'Hodor', 'position' => 'programmer'],
+    ['name' => 'Joker', 'position' => 'CEO'],
+    ['name' => 'Elvis', 'position' => 'CTO'],
+];
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () use ($team) {
+    return view('about', ['team' => $team]);
 });
